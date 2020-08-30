e4f20c5c0d6a5597fe8fa4ffa109fa5d1334b9d6
Kajal Bansal <kajal.bansal_cs19@gla.ac.in>
2020-08-30 09:17:07 +0530

    add parametrized constructor in Book.java

--- src/definitions/Book.java
+++ src/definitions/Book.java
@@ -8,13 +8,19 @@ package definitions;
 
 public class Book {
     private String nameOfTheBook;
-    private String nsmeOfTheAuthorOfTheBook;
+    private String nameOfTheAuthorOfTheBook;
     private String thirteenDigitISBNNumberOfTheBook;
 
 //non-parametrized constructor
 public Book() {
     this.nameOfTheBook = "null";
-    this.nsmeOfTheAuthorOfTheBook = "null";
+    this.nameOfTheAuthorOfTheBook = "null";
     this.thirteenDigitISBNNumberOfTheBook = "null";
+}
+//parametrized constructor
+public Book(String nameOfTheBook, String nameOfTheAuthorOfTheBook, String thirteenDigitISBNNumberOfTheBook){
+    this.nameOfTheBook = nameOfTheBook;
+    this.nameOfTheAuthorOfTheBook = nameOfTheAuthorOfTheBook;
+    this.thirteenDigitISBNNumberOfTheBook = thirteenDigitISBNNumberOfTheBook;
 }
 }
