009265ca25d7594fd392d3e7c23f5027a48acfa0
Kajal Bansal <kajal.bansal_cs19@gla.ac.in>
2020-08-30 12:40:38 +0530

    create Library.java

--- src/definitions/Student.java
+++ src/definitions/Student.java
@@ -139,7 +139,8 @@ public class Student {
             System.out.println("These are the books issued by you: ");
             for (int issuedBook = 0; issuedBook < MAXIMUM_NO_OF_BOOKS_CAN_ISSUE; issuedBook++) {
                 if (theNamesOfTheBooksIssuedByTheStudent[issuedBook].getNameOfTheBook() != null) {
-                    System.out.print(theNamesOfTheBooksIssuedByTheStudent[issuedBook].getNameOfTheBook() + (issuedBook < getNumberOfBooksIssuedByTheStudent() - 1 ? ", " : ".\n"));
+                    System.out.print(theNamesOfTheBooksIssuedByTheStudent[issuedBook].getNameOfTheBook()
+                            + (issuedBook < getNumberOfBooksIssuedByTheStudent() - 1 ? ", " : ".\n"));
                 }
             }
         }
