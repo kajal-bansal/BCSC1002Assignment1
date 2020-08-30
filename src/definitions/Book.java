782d612c93b7fbc0732722b86404b80636c0c619
Kajal Bansal <kajal.bansal_cs19@gla.ac.in>
2020-08-30 09:21:39 +0530

    create the equals and the hashcode method in Book.java

--- src/definitions/Book.java
+++ src/definitions/Book.java
@@ -6,6 +6,8 @@
  * */
 package definitions;
 
+import java.util.Objects;
+
 public class Book {
     private String nameOfTheBook;
     private String nameOfTheAuthorOfTheBook;
@@ -55,5 +57,20 @@ public Book(String nameOfTheBook, String nameOfTheAuthorOfTheBook, String thirte
                 ", nameOfTheAuthorOfTheBook='" + nameOfTheAuthorOfTheBook + '\'' +
                 ", thirteenDigitISBNNumberOfTheBook='" + thirteenDigitISBNNumberOfTheBook + '\'' +
                 '}';
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof Book)) return false;
+        Book book = (Book) o;
+        return Objects.equals(nameOfTheBook, book.nameOfTheBook) &&
+                Objects.equals(nameOfTheAuthorOfTheBook, book.nameOfTheAuthorOfTheBook) &&
+                Objects.equals(thirteenDigitISBNNumberOfTheBook, book.thirteenDigitISBNNumberOfTheBook);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(nameOfTheBook, nameOfTheAuthorOfTheBook, thirteenDigitISBNNumberOfTheBook);
     }
 }
