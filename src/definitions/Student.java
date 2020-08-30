2de3b0c5dbac8ebc45d05805691215b46b3c7f71
Kajal Bansal <kajal.bansal_cs19@gla.ac.in>
2020-08-30 09:53:07 +0530

    create non-parametrized constructor in Student.java

--- src/definitions/Student.java
+++ src/definitions/Student.java
@@ -6,9 +6,25 @@
  * */
 package definitions;
 
+import java.util.Arrays;
+import java.util.Objects;
+
 public class Student {
+    public static final int MAXIMUM_NO_OF_BOOKS_CAN_ISSUE = 4;
     private String nameOfTheStudent;
     private long universityRollNumberOfTheStudent;
     private int numberOfBooksIssuedByTheStudent;
     private Book[] theNamesOfTheBooksIssuedByTheStudent;
+
+    //non-parametrized constructor
+    public Student() {
+        this.nameOfTheStudent = "null";
+        this.universityRollNumberOfTheStudent = 0L;
+        this.numberOfBooksIssuedByTheStudent = 0;
+        this.theNamesOfTheBooksIssuedByTheStudent = new Book[MAXIMUM_NO_OF_BOOKS_CAN_ISSUE];
+        for (int i = 0; i < MAXIMUM_NO_OF_BOOKS_CAN_ISSUE; i++) {
+            this.theNamesOfTheBooksIssuedByTheStudent[i] = new Book();
+        }
+    }
+
 }
