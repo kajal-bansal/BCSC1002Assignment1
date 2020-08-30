bf8ef1e0f27a54cfef5580d91a4da48937d1a083
Kajal Bansal <kajal.bansal_cs19@gla.ac.in>
2020-08-30 12:48:39 +0530

    add set method in Library.java

--- src/definitions/Library.java
+++ src/definitions/Library.java
@@ -26,4 +26,7 @@ public class Library {
         return booksThatAreCurrentlyAvailable;
     }
 
+    public void setBooksThatAreCurrentlyAvailable(Book[] booksThatAreCurrentlyAvailable) {
+        this.booksThatAreCurrentlyAvailable = booksThatAreCurrentlyAvailable;
+    }
 }
